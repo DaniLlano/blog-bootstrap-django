1bdaadebae324bff82242ade2c5a740b2dfeabfa
Dani Llano <danillano97@gmail.com>
2023-10-10 17:38:17 -0300

    Update blog post urls and views

--- src/post/models.py
+++ src/post/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.urls import reverse
 
 User = get_user_model()
 
@@ -17,14 +18,21 @@ class Category(models.Model):
         return self.title
 
 class Post(models.Model):
-    title = models.CharField(max_length = 100)
+    title = models.CharField(max_length=100)
     overview = models.TextField()
-    timestamp = models.DateTimeField(auto_now_add = True)
-    comment_count = models.IntegerField(default = 0)
-    author = models.ForeignKey(Author, on_delete = models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    comment_count = models.IntegerField(default=0)
+    view_count = models.IntegerField(default=0)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
     thumbnail = models.ImageField()
     categories = models.ManyToManyField(Category)
     featured = models.BooleanField()
 
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={
+            'id': self.id
+        })
+    
