302e304fb3da91adec49a6f5cbecba7e4251ed69
Dani Llano <danillano97@gmail.com>
2023-10-10 17:51:28 -0300

    Add category count to blog page context

--- src/post/views.py
+++ src/post/views.py
@@ -1,7 +1,17 @@
+from django.db.models import Count
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
 from .models import Post
 from marketing.models import Signup
+
+def get_category_count():
+    # Get all posts with their categories
+    # And count the number of posts in each category
+    queryset = Post \
+        .objects \
+        .values('categories__title') \
+        .annotate(Count('categories__title'))
+    return queryset
 
 def index(request):
     featured = Post.objects.filter(featured = True)
@@ -20,6 +30,7 @@ def index(request):
     return render(request, 'index.html', context)
 
 def blog(request):
+    category_count = get_category_count()
     most_recent = Post.objects.order_by('-timestamp')[:3]
     post_list = Post.objects.all()
     paginator = Paginator(post_list, 4)
@@ -37,6 +48,7 @@ def blog(request):
         'queryset': paginated_queryset,
         'most_recent': most_recent,
         'page_request_var': page_request_var,
+        'category_count': category_count,
     }
     return render(request, 'blog.html', context)
 
